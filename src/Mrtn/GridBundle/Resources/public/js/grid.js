9e67806220a6754606efdd7aeebcdb5a4f964841
mikemirten <mikemirten@gmail.com>
2016-05-08 17:04:05 -0400

    grid.js rewrited to jQuery plugin

--- src/Mrtn/GridBundle/Resources/public/js/grid.js
+++ src/Mrtn/GridBundle/Resources/public/js/grid.js
@@ -1,10 +1,15 @@
-$(function() {
-	var params    = {};
-	var container = $('#zgrid_container');
-	var gridUrl   = container.attr('data-url');
+(function($) {
+	/**
+	 * Multiple action state
+	 * 
+	 * @type Boolean
+	 */
 	var multiple  = false;
-	
-	// Code 17: Ctrl key
+
+	/**
+	 * Multiple actions detector
+	 * Code 17: Ctrl key
+	 */
 	$(document).keydown(function(event) {
 		if (event.which === 17) {
 			multiple = true;
@@ -14,7 +19,7 @@ $(function() {
 			multiple = false;
 		}
 	});
-	
+
 	/**
 	 * Parse action's definition
 	 * 
@@ -29,11 +34,11 @@ $(function() {
 	var parseDefinition = function(src)
 	{
 		var matches = src.match(/^([a-zA-Z0-9_]+)\.([a-zA-Z0-9_]+)\(([a-zA-Z0-9_]+):([^\)]+)\)$/);
-		
+
 		if (matches === null) {
 			throw 'Invalid definition: "' + src + '"';
 		}
-		
+
 		return {
 			param:  matches[1],
 			action: matches[2],
@@ -41,7 +46,7 @@ $(function() {
 			value:  matches[4]
 		};
 	};
-	
+
 	/**
 	 * Process value
 	 * 
@@ -51,19 +56,43 @@ $(function() {
 	var processValue = function(value)
 	{
 		var matches = value.match(/^\[(.+?)\]$/);
-		
+
 		if (matches === null) {
 			return value;
 		}
-		
+
 		return $(matches[1]).val();
 	};
-	
+
+	/**
+	 * Assemble request string from parameters set
+	 * 
+	 * @param   {Object} params
+	 * @returns {String}
+	 */
+	var assembleRequest = function(params)
+	{
+		var paramsList = [];
+
+		for (var paramKey in params) {
+			var param = params[paramKey];
+			var fieldList = [];
+
+			for (var fieldKey in param) {
+				fieldList.push(fieldKey + ':' + param[fieldKey]);
+			}
+
+			paramsList.push(paramKey + '=' + fieldList.join(','));
+		}
+
+		return paramsList.join('&');
+	};
+
 	/**
 	 * Process action (apply to parameters)
 	 * 
-	 * @param   {Object} params
-	 * @param   {Object} action
+	 * @param {Object} params
+	 * @param {Object} action
 	 */
 	var processAction = function(params, action)
 	{
@@ -76,83 +105,87 @@ $(function() {
 			} else {
 				params[action.param] = {};
 			}
-			
+
 			params[action.param][action.field] = action.value;
 			return;
 		}
-	
+
 		// Add value to parameter
 		if (action.action === 'set') {
 			if (typeof params[action.param] === 'undefined') {
 				params[action.param] = {};
 			}
-			
+
 			params[action.param][action.field] = action.value;
 			return;
 		}
-		
+
 		// Remove value from parameter
 		if (action.action === 'remove') {
 			if (typeof params[action.param] === 'undefined') {
 				return;
 			}
-			
+
 			delete params[action.param][action.field];
-			
+
 			if ($.isEmptyObject(params[action.param])) {
 				delete params[action.param];
 			}
 		}
 	};
-	
+
 	/**
-	 * Assemble request string from parameters set
+	 * Grid object
 	 * 
-	 * @param   {Object} params
-	 * @returns {String}
+	 * @returns {Grid}
 	 */
-	var assembleRequest = function(params)
-	{
-		var paramsList = [];
+	var Grid = function(container)
+	{		
+		this.dataParams = {};
+		this.container  = container;
+		this.dataUrl    = this.container.attr('data-url');
 		
-		for (var paramKey in params) {
-			var param = params[paramKey];
-			var fieldList = [];
-			
-			for (var fieldKey in param) {
-				fieldList.push(fieldKey + ':' + param[fieldKey]);
+		var self = this;
+		
+		/**
+		 * Handle parameter's action
+		 */
+		this.parametersHandler = function()
+		{
+			var definition = $(this).attr('data-definition');
+			var action     = parseDefinition(definition);
+
+			action.value = processValue(action.value);
+
+			if (action.value === '') {
+				return;
 			}
-			
-			paramsList.push(paramKey + '=' + fieldList.join(','));
-		}
+
+			processAction(self.dataParams, action);
+
+			$.ajax({
+				url: self.dataUrl + '?' + assembleRequest(self.dataParams),
+				success: function(response) {
+					self.container.html(response);
+					self.container.find('.js-zgrid-param').click(self.parametersHandler);
+				}
+			});
+		};
 		
-		return paramsList.join('&');
+		this.container.find('.js-zgrid-param').click(this.parametersHandler);
 	};
-	
+
 	/**
-	 * Handle parameter's action
+	 * jQuery plugin init
+	 * 
+	 * @returns {Grid}
 	 */
-	var parametersHandler = function()
+	$.fn.zgrid = function()
 	{
-		var definition = $(this).attr('data-definition');
-		var action     = parseDefinition(definition);
-		
-		action.value = processValue(action.value);
-		
-		if (action.value === '') {
-			return;
-		}
-		
-		processAction(params, action);
-		
-		$.ajax({
-			url: gridUrl + '?' + assembleRequest(params),
-			success: function(response) {
-				container.html(response);
-				container.find('.js-zgrid-param').click(parametersHandler);
-			}
+		this.each(function() {
+			new Grid($(this));
 		});
+		
+		return this;
 	};
-	
-	$('.js-zgrid-param').click(parametersHandler);
-});
+})(jQuery);
